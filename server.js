f5fa4459dece59b49d9b0d74912234a36743e2a1
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-07-06 11:21:03 -0600

    added vue and linked style sheet to index.html

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ mongoose.connect('mongodb://localhost/superheroes');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req,res){
   Superhero.find(function(err, superheroes){
     if (err)
