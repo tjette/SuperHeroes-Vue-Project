8497c3aad5d3e360945af864d345e1cc04e13337
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-07-12 12:57:22 -0600

    made changes to delete route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-//var mainRoutes = require('./routes/main');
+
 var heroRoutes = require('./routes/superheroes');
 var Superhero = require('./models/Superhero')
 var app = express();
@@ -11,20 +11,10 @@ mongoose.connect('mongodb://localhost/superheroes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-//app.use('/api/main', mainRoutes);
+
 app.use('/api/heroes', heroRoutes);
 
 app.use(express.static(__dirname + '/public'));
-
-app.delete('/api/heroes/:_id', function(req,res){
-  Superhero.remove({_id: req.params._id }, function(err){
-    if(err){
-      res.send("Error", err)
-    } else {
-      res.send("Superhero deleted");
-    }
-  })
-})
 
 var server = app.listen(port, function(){
   console.log("Listening on port", port);
