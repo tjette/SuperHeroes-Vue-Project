131b3f4b9bbf987f054f23a97a6856da4878f4c6
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-07-14 11:45:18 -0600

    have villain get and post route working

--- routes/superheroes.js
+++ routes/superheroes.js
@@ -2,27 +2,33 @@ var express = require('express');
 var Router = express.Router();
 var Superhero = require('../models/Superhero');
 
-Router.route('/').get(function(req,res){
-  Superhero.find(function(err, superheroes){
-    if(err){
-      res.send("Error");
-    } else {
-      res.json({data: superheroes});
-    }
-  })
-}).post(function(req,res){
-  console.log("in the post route");
-  var superhero = new Superhero();
-  superhero.name = req.body.name;
-  superhero.superPower = req.body.superPower;
-  superhero.img = req.body.img;
+Router.route('/')
 
-  superhero.save().then(function(superhero){
-    res.json({message: "Hero created", data: superhero});
-  }, function(err){
-    res.send(err);
+  .get(function(req,res){
+    Superhero.find(function(err, superheroes){
+      if(err){
+        res.send("Error");
+      } else {
+        res.json({data: superheroes});
+      }
+    })
   })
-})
+
+  .post(function(req,res){
+    console.log("in the post route");
+    var superhero = new Superhero();
+    superhero.name = req.body.name;
+    superhero.superPower = req.body.superPower;
+    superhero.img = req.body.img;
+
+    superhero.save(function(err, superhero){
+      if(err){
+        res.send(err, "error");
+      } else {
+        res.json({message: "Superhero posted", data: superhero})
+      }
+    });
+  });
 
 Router.route('/:_id').get(function(req,res){
   Superhero.findById(req.params._id, function(err, superhero) {
--- routes/villains.js
+++ routes/villains.js
@@ -3,4 +3,33 @@ var Router = express.Router();
 var Villain = require('../models/Villain');
 
 
+Router.route('/')
+
+  .get(function(req,res){
+    Villain.find(function(err,villains){
+      if(err){
+        res.send('Error');
+      } else {
+        res.json({message: "Villains retrieved", data: villains});
+      }
+    })
+  })
+
+  .post(function(req,res){
+    console.log("in the post route");
+
+    var villain = new Villain();
+    villain.name = req.body.name;
+    villain.evilPower = req.body.evilPower;
+    villain.img = req.body.img;
+
+    villain.save(function(err,villain){
+      if(err){
+        res.send(err, "error");
+      } else {
+        res.json({message: "Villain has been posted", data: villain})
+      }
+    });
+  });
+
 module.exports = Router;
